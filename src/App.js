58afc4460a51d6ddebfa1eaa3b7ad2fba5dd2c29
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-10-17 01:56:50 +0200

    add layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Login from './screens/Login'
 function App() {
   return (
     <div className="App">
-      Would You rather?
+      <Login />
     </div>
   );
 }
---
+++ src/screens/NewQuestion.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Newquestion extends Component {
+    render() {
+        return (
+            <div>ADD QUESTION</div>
+        )
+    }
+}
+
+export default Newquestion
