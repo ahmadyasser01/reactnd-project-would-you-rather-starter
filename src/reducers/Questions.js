cbd9ff89e810c3730f366a237f4c98611b1dbb85
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-10-18 00:24:37 +0200

    working on reducers

---
+++ src/reducers/Questions.js
@@ -0,0 +1,35 @@
+import { ADD_QUESTION, ANSWER_QUESTION, RECIEVE_QUESTIONS } from "../actions/Questions";
+
+export default function questions(state = {}, action) {
+    switch (action.type) {
+        case RECIEVE_QUESTIONS:
+            return {
+                ...state,
+                ...action.questions
+            }
+        case ANSWER_QUESTION:
+            const { questionId, answer, authedUser } = action
+            const SelectedOption = state[questionId][answer]
+            return {
+                ...state,
+                [questionId]: {
+                    ...state[questionId],
+                    [answer]: {
+                        questionId,
+                        votes: [...SelectedOption.votes,
+                            authedUser]
+                    }
+                }
+
+            }
+        case ADD_QUESTION:
+            return {
+                ...questions,
+                [action.question.id]: action.question
+            }
+
+
+        default:
+            return state;
+    }
+}
